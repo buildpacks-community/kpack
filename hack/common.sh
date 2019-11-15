ec3fd59d94dcb40b0a837af48b6bedbef0903487
Matthew McNew <mmcnew@pivotal.io>
2019-11-15 11:49:18 -0600

    Utilize cloudfoundry/cnb:tiny for kpack images

--- hack/common.sh
+++ hack/common.sh
@@ -3,7 +3,7 @@
 function pack_build() {
     image=$1
     target=$2
-    builder="cloudfoundry/cnb:bionic"
+    builder="cloudfoundry/cnb:tiny"
 
     pack build ${image} --builder ${builder} -e BP_GO_TARGETS=${target} --publish
 
