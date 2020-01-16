5657365bb942e8a04f75fb89d3964779a3e38ab4
Matthew McNew <mmcnew@pivotal.io>
2020-01-16 10:34:54 -0500

    hardcode kpack version
    - An existing bug in the go buildpack fails to set LD Flags
    
    Signed-off-by: Ashwin Venkatesh <avenkatesh@pivotal.io>

--- cmd/version.go
+++ cmd/version.go
@@ -2,5 +2,5 @@ package cmd
 
 // This is set by CI when creating release binaries
 var (
-	Version = "0.0.0"
+	Version = "v0.0.6" //Temporarily: https://github.com/cloudfoundry/go-mod-cnb/issues/12
 )
