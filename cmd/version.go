3fcd05a0a5d9fa6c4438614d81b50fec5320d4b9
Matthew McNew <mmcnew@pivotal.io>
2020-01-17 10:42:16 -0700

    Version set at build time

--- cmd/version.go
+++ cmd/version.go
@@ -2,5 +2,5 @@ package cmd
 
 // This is set by CI when creating release binaries
 var (
-	Version = "v0.0.6" //Temporarily: https://github.com/cloudfoundry/go-mod-cnb/issues/12
+	Version = "v0.0.0"
 )
