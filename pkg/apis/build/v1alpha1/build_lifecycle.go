d4cc9f2aa35bf03e0e277dce1fb3c3f046adbd16
Matthew McNew <mmcnew@pivotal.io>
2020-01-23 11:51:29 -0600

    Cowardly refuse to attempt builds with a platform api > 0.2
    - Save build pod creation error in build status

---
+++ pkg/apis/build/v1alpha1/build_lifecycle.go
@@ -0,0 +1,18 @@
+package v1alpha1
+
+import (
+	corev1alpha1 "github.com/pivotal/kpack/pkg/apis/core/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (bs *BuildStatus) Error(err error) {
+	bs.Conditions = corev1alpha1.Conditions{
+		{
+			LastTransitionTime: corev1alpha1.VolatileTime{Inner: metav1.Now()},
+			Type:               corev1alpha1.ConditionSucceeded,
+			Status:             corev1.ConditionFalse,
+			Message:            err.Error(),
+		},
+	}
+}
--- pkg/apis/build/v1alpha1/build_test.go
+++ pkg/apis/build/v1alpha1/build_test.go
@@ -1,10 +1,15 @@
 package v1alpha1
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	corev1alpha1 "github.com/pivotal/kpack/pkg/apis/core/v1alpha1"
 )
 
 func TestRebaseable(t *testing.T) {
@@ -43,4 +48,30 @@ func TestRebaseable(t *testing.T) {
 	}
 	require.True(t, build.rebasable("matching.stack"))
 
+}
+
+func TestBuildLifecycle(t *testing.T) {
+	build := &Build{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-name",
+		},
+	}
+	build.Status.Error(errors.New("error: display this error"))
+
+	require.True(t, equality.Semantic.DeepEqual(build, &Build{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-name",
+		},
+		Status: BuildStatus{
+			Status: corev1alpha1.Status{
+				Conditions: corev1alpha1.Conditions{
+					{
+						Type:    corev1alpha1.ConditionSucceeded,
+						Status:  corev1.ConditionFalse,
+						Message: "error: display this error",
+					},
+				},
+			},
+		},
+	}))
 }
