54cf2accd815286fe62f8443aac14caf368fa4ce
Matthew McNew <mmcnew@pivotal.io>
2020-03-09 18:51:45 -0500

    Add blob fetch support for tar/tar.gz
    - Add blob fetch unit tests

--- pkg/blob/fetch.go
+++ pkg/blob/fetch.go
@@ -1,7 +1,9 @@
 package blob
 
 import (
+	"archive/tar"
 	"archive/zip"
+	"compress/gzip"
 	"io"
 	"io/ioutil"
 	"log"
@@ -38,6 +40,73 @@ func (f *Fetcher) Fetch(dir string, blobURL string) error {
 		return err
 	}
 
+	fileType, err := classifyFile(file.Name())
+	if err != nil {
+		return err
+	}
+
+	switch fileType {
+	case "application/zip":
+		err = extractZip(file, dir)
+	case "application/x-gzip":
+		r, err := os.Open(file.Name())
+		if err != nil {
+			return err
+		}
+
+		gzf, err := gzip.NewReader(r)
+		if err != nil {
+			return err
+		}
+
+		err = extractTar(gzf, dir)
+	case "application/octet-stream":
+		r, err := os.Open(file.Name())
+		if err != nil {
+			return err
+		}
+		err = extractTar(r, dir)
+	}
+
+	if err != nil {
+		return err
+	}
+	f.Logger.Printf("Successfully downloaded %s in path %q", blob.Host+blob.Path, dir)
+	return nil
+}
+
+func extractTar(reader io.Reader, dir string) error {
+	tarReader := tar.NewReader(reader)
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		filePath := filepath.Join(dir, header.Name)
+		switch header.Typeflag {
+		case tar.TypeDir:
+			err := os.MkdirAll(filePath, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+		case tar.TypeReg:
+			outFile, err := os.Create(filePath)
+			if err != nil {
+				return err
+			}
+			defer outFile.Close()
+			if _, err := io.Copy(outFile, tarReader); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func extractZip(file *os.File, dir string) error {
 	zipReader, err := zip.OpenReader(file.Name())
 	if err != nil {
 		return err
@@ -68,15 +137,30 @@ func (f *Fetcher) Fetch(dir string, blobURL string) error {
 			return err
 		}
 
-		_, err = io.Copy(outFile, srcFile)
+		if _, err := io.Copy(outFile, srcFile); err != nil {
+			return err
+		}
 
 		outFile.Close()
 		srcFile.Close()
-
-		if err != nil {
-			return err
-		}
 	}
-	f.Logger.Printf("Successfully downloaded %s in path %q", blob.Host+blob.Path, dir)
 	return nil
+}
+
+func classifyFile(f string) (string, error) {
+	file, err := os.Open(f)
+	if err != nil {
+		return "", err
+	}
+
+	defer file.Close()
+
+	buff := make([]byte, 512)
+	_, err = file.Read(buff)
+
+	if err != nil {
+		return "", err
+	}
+
+	return http.DetectContentType(buff), nil
 }
