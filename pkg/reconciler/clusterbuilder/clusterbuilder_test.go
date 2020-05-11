82b501d97f99e62624f11458ae7dddb7d0e6b11f
Matthew McNew <mmcnew@pivotal.io>
2020-05-11 09:34:41 -0500

    Remove unnecessary version hierarchy in reconciler paths

---
+++ pkg/reconciler/clusterbuilder/clusterbuilder_test.go
@@ -0,0 +1,306 @@
+package clusterbuilder_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgotesting "k8s.io/client-go/testing"
+	"k8s.io/client-go/tools/record"
+	"knative.dev/pkg/controller"
+	rtesting "knative.dev/pkg/reconciler/testing"
+
+	"github.com/pivotal/kpack/pkg/apis/build/v1alpha1"
+	corev1alpha1 "github.com/pivotal/kpack/pkg/apis/core/v1alpha1"
+	"github.com/pivotal/kpack/pkg/client/clientset/versioned/fake"
+	"github.com/pivotal/kpack/pkg/reconciler/clusterbuilder"
+	"github.com/pivotal/kpack/pkg/reconciler/clusterbuilder/clusterbuilderfakes"
+	"github.com/pivotal/kpack/pkg/reconciler/testhelpers"
+)
+
+func TestClusterBuilderReconciler(t *testing.T) {
+	spec.Run(t, "Cluster Builder Reconciler", testClusterBuilderReconciler)
+}
+
+func testClusterBuilderReconciler(t *testing.T, when spec.G, it spec.S) {
+	fakeMetadataRetriever := &clusterbuilderfakes.FakeMetadataRetriever{}
+
+	fakeEnqueuer := &clusterbuilderfakes.FakeEnqueuer{}
+
+	rt := testhelpers.ReconcilerTester(t,
+		func(t *testing.T, row *rtesting.TableRow) (reconciler controller.Reconciler, lists rtesting.ActionRecorderList, list rtesting.EventList, reporter *rtesting.FakeStatsReporter) {
+			listers := testhelpers.NewListers(row.Objects)
+
+			fakeClient := fake.NewSimpleClientset(listers.BuildServiceObjects()...)
+
+			eventRecorder := record.NewFakeRecorder(10)
+			actionRecorderList := rtesting.ActionRecorderList{fakeClient}
+			eventList := rtesting.EventList{Recorder: eventRecorder}
+			r := &clusterbuilder.Reconciler{
+				Client:               fakeClient,
+				ClusterBuilderLister: listers.GetClusterBuilderLister(),
+				MetadataRetriever:    fakeMetadataRetriever,
+				Enqueuer:             fakeEnqueuer,
+			}
+
+			return r, actionRecorderList, eventList, &rtesting.FakeStatsReporter{}
+		})
+
+	const (
+		clusterBuilderName             = "cluster-builder-name"
+		key                            = "some-namespace/builder-name"
+		clusterBuilderKey              = "cluster-builder-name"
+		clusterImageName               = "some/cluster-builder"
+		clusterBuilderIdentifier       = "some/cluster-builder@sha256:resolved-builder-digest"
+		initialGeneration        int64 = 1
+	)
+
+	clusterBuilder := &v1alpha1.ClusterBuilder{
+		ObjectMeta: v1.ObjectMeta{
+			Name:       clusterBuilderName,
+			Generation: initialGeneration,
+		},
+		Spec: v1alpha1.BuilderSpec{
+			Image:        clusterImageName,
+			UpdatePolicy: "polling",
+		},
+	}
+
+	when("#Reconcile", func() {
+		when("cluster builder", func() {
+			when("metadata is available", func() {
+				fakeMetadataRetriever.GetBuilderImageReturns(v1alpha1.BuilderRecord{
+					Image: clusterBuilderIdentifier,
+					Stack: v1alpha1.BuildStack{
+						RunImage: "",
+						ID:       "",
+					},
+					Buildpacks: v1alpha1.BuildpackMetadataList{
+						{
+							Id:      "buildpack.version",
+							Version: "version",
+						},
+					},
+				}, nil)
+
+				it("saves metadata to the status", func() {
+					testBuilder := &v1alpha1.ClusterBuilder{
+						ObjectMeta: clusterBuilder.ObjectMeta,
+						Spec:       clusterBuilder.Spec,
+						Status: v1alpha1.BuilderStatus{
+							Status: corev1alpha1.Status{
+								ObservedGeneration: 1,
+								Conditions: corev1alpha1.Conditions{
+									{
+										Type:   corev1alpha1.ConditionReady,
+										Status: corev1.ConditionTrue,
+									},
+								},
+							},
+							BuilderMetadata: []v1alpha1.BuildpackMetadata{
+								{
+									Id:      "buildpack.version",
+									Version: "version",
+								},
+							},
+							LatestImage: clusterBuilderIdentifier,
+						},
+					}
+					rt.Test(rtesting.TableRow{
+						Key:     clusterBuilderKey,
+						Objects: []runtime.Object{clusterBuilder},
+						WantErr: false,
+						WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+							{
+								Object: testBuilder,
+							},
+						},
+					})
+
+					require.Equal(t, fakeMetadataRetriever.GetBuilderImageCallCount(), 1)
+				})
+
+				it("schedule next polling when update policy is not set", func() {
+					rt.Test(rtesting.TableRow{
+						Key:     clusterBuilderKey,
+						Objects: []runtime.Object{clusterBuilder},
+						WantErr: false,
+						WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+							{
+								Object: &v1alpha1.ClusterBuilder{
+									ObjectMeta: clusterBuilder.ObjectMeta,
+									Spec:       clusterBuilder.Spec,
+									Status: v1alpha1.BuilderStatus{
+										Status: corev1alpha1.Status{
+											ObservedGeneration: 1,
+											Conditions: corev1alpha1.Conditions{
+												{
+													Type:   corev1alpha1.ConditionReady,
+													Status: corev1.ConditionTrue,
+												},
+											},
+										},
+										BuilderMetadata: []v1alpha1.BuildpackMetadata{
+											{
+												Id:      "buildpack.version",
+												Version: "version",
+											},
+										},
+										LatestImage: clusterBuilderIdentifier,
+									},
+								},
+							},
+						},
+					})
+					assert.Equal(t, 1, fakeEnqueuer.EnqueueCallCount())
+				})
+
+				it("does schedule polling when update policy is set to polling", func() {
+					clusterBuilder.Spec.UpdatePolicy = v1alpha1.Polling
+					rt.Test(rtesting.TableRow{
+						Key:     clusterBuilderKey,
+						Objects: []runtime.Object{clusterBuilder},
+						WantErr: false,
+						WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+							{
+								Object: &v1alpha1.ClusterBuilder{
+									ObjectMeta: clusterBuilder.ObjectMeta,
+									Spec:       clusterBuilder.Spec,
+									Status: v1alpha1.BuilderStatus{
+										Status: corev1alpha1.Status{
+											ObservedGeneration: 1,
+											Conditions: corev1alpha1.Conditions{
+												{
+													Type:   corev1alpha1.ConditionReady,
+													Status: corev1.ConditionTrue,
+												},
+											},
+										},
+										BuilderMetadata: []v1alpha1.BuildpackMetadata{
+											{
+												Id:      "buildpack.version",
+												Version: "version",
+											},
+										},
+										LatestImage: clusterBuilderIdentifier,
+									},
+								},
+							},
+						},
+					})
+					assert.Equal(t, 1, fakeEnqueuer.EnqueueCallCount())
+				})
+
+				it("does not schedule polling when update policy is set to external", func() {
+					clusterBuilder.Spec.UpdatePolicy = v1alpha1.External
+					rt.Test(rtesting.TableRow{
+						Key:     clusterBuilderKey,
+						Objects: []runtime.Object{clusterBuilder},
+						WantErr: false,
+						WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+							{
+								Object: &v1alpha1.ClusterBuilder{
+									ObjectMeta: clusterBuilder.ObjectMeta,
+									Spec:       clusterBuilder.Spec,
+									Status: v1alpha1.BuilderStatus{
+										Status: corev1alpha1.Status{
+											ObservedGeneration: 1,
+											Conditions: corev1alpha1.Conditions{
+												{
+													Type:   corev1alpha1.ConditionReady,
+													Status: corev1.ConditionTrue,
+												},
+											},
+										},
+										BuilderMetadata: []v1alpha1.BuildpackMetadata{
+											{
+												Id:      "buildpack.version",
+												Version: "version",
+											},
+										},
+										LatestImage: clusterBuilderIdentifier,
+									},
+								},
+							},
+						},
+					})
+
+					assert.Equal(t, 0, fakeEnqueuer.EnqueueCallCount())
+				})
+
+				it("does not update the status with no status change", func() {
+					clusterBuilder.Status = v1alpha1.BuilderStatus{
+						Status: corev1alpha1.Status{
+							ObservedGeneration: clusterBuilder.Generation,
+							Conditions: corev1alpha1.Conditions{
+								{
+									Type:   corev1alpha1.ConditionReady,
+									Status: corev1.ConditionTrue,
+								},
+							},
+						},
+						BuilderMetadata: []v1alpha1.BuildpackMetadata{
+							{
+								Id:      "buildpack.version",
+								Version: "version",
+							},
+						},
+						LatestImage: clusterBuilderIdentifier,
+					}
+
+					rt.Test(rtesting.TableRow{
+						Key:     clusterBuilderKey,
+						Objects: []runtime.Object{clusterBuilder},
+						WantErr: false,
+					})
+				})
+			})
+
+			when("metadata is not available", func() {
+				fakeMetadataRetriever.GetBuilderImageReturns(v1alpha1.BuilderRecord{}, errors.New("unavailable metadata"))
+
+				it("saves not ready to the builder status", func() {
+					rt.Test(rtesting.TableRow{
+						Key:     clusterBuilderKey,
+						Objects: []runtime.Object{clusterBuilder},
+						WantErr: true,
+						WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+							{
+								Object: &v1alpha1.ClusterBuilder{
+									ObjectMeta: clusterBuilder.ObjectMeta,
+									Spec:       clusterBuilder.Spec,
+									Status: v1alpha1.BuilderStatus{
+										Status: corev1alpha1.Status{
+											ObservedGeneration: 1,
+											Conditions: corev1alpha1.Conditions{
+												{
+													Type:    corev1alpha1.ConditionReady,
+													Status:  corev1.ConditionFalse,
+													Message: "unavailable metadata",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					})
+
+					assert.Equal(t, fakeEnqueuer.EnqueueCallCount(), 1)
+				})
+			})
+		})
+
+		it("does not return error on nonexistent builder", func() {
+			rt.Test(rtesting.TableRow{
+				Key:     key,
+				WantErr: false,
+			})
+		})
+	})
+}
---
+++ pkg/reconciler/sourceresolver/sourceresolver_test.go
@@ -0,0 +1,462 @@
+package sourceresolver_test
+
+import (
+	"testing"
+
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	k8sfake "k8s.io/client-go/kubernetes/fake"
+	clientgotesting "k8s.io/client-go/testing"
+	"k8s.io/client-go/tools/record"
+	"knative.dev/pkg/controller"
+	rtesting "knative.dev/pkg/reconciler/testing"
+
+	"github.com/pivotal/kpack/pkg/apis/build/v1alpha1"
+	corev1alpha1 "github.com/pivotal/kpack/pkg/apis/core/v1alpha1"
+	"github.com/pivotal/kpack/pkg/client/clientset/versioned/fake"
+	"github.com/pivotal/kpack/pkg/reconciler/sourceresolver"
+	"github.com/pivotal/kpack/pkg/reconciler/sourceresolver/sourceresolverfakes"
+	"github.com/pivotal/kpack/pkg/reconciler/testhelpers"
+)
+
+func TestSourceResolver(t *testing.T) {
+	spec.Run(t, "Source Resolver Reconciler", testSourceResolver)
+}
+
+func testSourceResolver(t *testing.T, when spec.G, it spec.S) {
+	const (
+		sourceResolverName       = "source-resolver-name"
+		namespace                = "some-namespace"
+		key                      = "some-namespace/source-resolver-name"
+		originalGeneration int64 = 0
+		serviceAccount           = "serviceAccount"
+	)
+
+	fakeGitResolver := &sourceresolverfakes.FakeResolver{}
+	fakeBlobResolver := &sourceresolverfakes.FakeResolver{}
+	fakeRegistryResolver := &sourceresolverfakes.FakeResolver{}
+	fakeEnqueuer := &sourceresolverfakes.FakeEnqueuer{}
+
+	rt := testhelpers.ReconcilerTester(t,
+		func(t *testing.T, row *rtesting.TableRow) (reconciler controller.Reconciler, lists rtesting.ActionRecorderList, list rtesting.EventList, reporter *rtesting.FakeStatsReporter) {
+			listers := testhelpers.NewListers(row.Objects)
+
+			fakeClient := fake.NewSimpleClientset(listers.BuildServiceObjects()...)
+			k8sfakeClient := k8sfake.NewSimpleClientset(listers.GetKubeObjects()...)
+
+			eventRecorder := record.NewFakeRecorder(10)
+			actionRecorderList := rtesting.ActionRecorderList{fakeClient, k8sfakeClient}
+			eventList := rtesting.EventList{Recorder: eventRecorder}
+
+			r := &sourceresolver.Reconciler{
+				GitResolver:          fakeGitResolver,
+				BlobResolver:         fakeBlobResolver,
+				RegistryResolver:     fakeRegistryResolver,
+				Enqueuer:             fakeEnqueuer,
+				Client:               fakeClient,
+				SourceResolverLister: listers.GetSourceResolverLister(),
+			}
+
+			rtesting.PrependGenerateNameReactor(&fakeClient.Fake)
+
+			return r, actionRecorderList, eventList, &rtesting.FakeStatsReporter{}
+		})
+
+	when("#Reconcile", func() {
+		when("a git based source config", func() {
+			sourceResolver := &v1alpha1.SourceResolver{
+				ObjectMeta: v1.ObjectMeta{
+					Name:       sourceResolverName,
+					Namespace:  namespace,
+					Generation: originalGeneration,
+				},
+				Spec: v1alpha1.SourceResolverSpec{
+					ServiceAccount: serviceAccount,
+					Source: v1alpha1.SourceConfig{
+						Git: &v1alpha1.Git{
+							URL:      "https://github.com/build-me",
+							Revision: "1234",
+						},
+					},
+				},
+			}
+
+			resolvedSource := v1alpha1.ResolvedSourceConfig{
+				Git: &v1alpha1.ResolvedGitSource{
+					URL:      "https://example.com/something",
+					Revision: "abcdef",
+					Type:     v1alpha1.Branch,
+				},
+			}
+
+			fakeGitResolver.ResolveReturns(resolvedSource, nil)
+			fakeGitResolver.CanResolveReturns(true)
+
+			it("updates the observed generation", func() {
+				sourceResolver := resolvedSourceResolver(sourceResolver, resolvedSource)
+				sourceResolver.Generation = 2
+
+				rt.Test(rtesting.TableRow{
+					Key: key,
+					Objects: []runtime.Object{
+						sourceResolver,
+					},
+					WantErr: false,
+					WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+						{
+							Object: &v1alpha1.SourceResolver{
+								ObjectMeta: sourceResolver.ObjectMeta,
+								Spec:       sourceResolver.Spec,
+								Status: v1alpha1.SourceResolverStatus{
+									Status: corev1alpha1.Status{
+										ObservedGeneration: 2,
+										Conditions:         sourceResolver.Status.Conditions,
+									},
+									Source: sourceResolver.Status.Source,
+								},
+							},
+						},
+					},
+				})
+			})
+
+			it("does not unnecessarily update the resource", func() {
+				sourceResolver := resolvedSourceResolver(sourceResolver, resolvedSource)
+
+				rt.Test(rtesting.TableRow{
+					Key: key,
+					Objects: []runtime.Object{
+						sourceResolver,
+					},
+					WantErr: false,
+				})
+			})
+
+			when("a branch is the source", func() {
+				resolvedSource := v1alpha1.ResolvedSourceConfig{
+					Git: &v1alpha1.ResolvedGitSource{
+						URL:      "https://example.com/something",
+						Revision: "abcdef",
+						Type:     v1alpha1.Branch,
+					},
+				}
+
+				fakeGitResolver.ResolveReturns(resolvedSource, nil)
+				fakeGitResolver.CanResolveReturns(true)
+
+				it("resolves git with the resolved source resolver", func() {
+					rt.Test(rtesting.TableRow{
+						Key: key,
+						Objects: []runtime.Object{
+							sourceResolver,
+						},
+						WantErr: false,
+						WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+							{
+								Object: &v1alpha1.SourceResolver{
+									ObjectMeta: sourceResolver.ObjectMeta,
+									Spec:       sourceResolver.Spec,
+									Status: v1alpha1.SourceResolverStatus{
+										Status: corev1alpha1.Status{
+											ObservedGeneration: originalGeneration,
+											Conditions: corev1alpha1.Conditions{
+												{
+													Type:   corev1alpha1.ConditionReady,
+													Status: corev1.ConditionTrue,
+												},
+												{
+													Type:   v1alpha1.ActivePolling,
+													Status: corev1.ConditionTrue,
+												},
+											},
+										},
+										Source: v1alpha1.ResolvedSourceConfig{
+											Git: &v1alpha1.ResolvedGitSource{
+												URL:      "https://example.com/something",
+												Revision: "abcdef",
+												Type:     v1alpha1.Branch,
+											},
+										},
+									},
+								},
+							},
+						},
+					})
+
+					require.Equal(t, 1, fakeEnqueuer.EnqueueCallCount())
+					enquedSourceResolver := fakeEnqueuer.EnqueueArgsForCall(0)
+					require.Equal(t, sourceResolver.Name, enquedSourceResolver.Name)
+					require.Equal(t, sourceResolver.Namespace, enquedSourceResolver.Namespace)
+				})
+			})
+
+			when("a specific commit sha is the source", func() {
+				resolvedSource := v1alpha1.ResolvedSourceConfig{
+					Git: &v1alpha1.ResolvedGitSource{
+						URL:      "https://example.com/something",
+						Revision: "abcdef",
+						Type:     v1alpha1.Commit,
+					},
+				}
+
+				fakeGitResolver.ResolveReturns(resolvedSource, nil)
+				fakeGitResolver.CanResolveReturns(true)
+
+				it("reconciles to ready and not active polling", func() {
+					rt.Test(rtesting.TableRow{
+						Key: key,
+						Objects: []runtime.Object{
+							sourceResolver,
+						},
+						WantErr: false,
+						WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+							{
+								Object: &v1alpha1.SourceResolver{
+									ObjectMeta: sourceResolver.ObjectMeta,
+									Spec:       sourceResolver.Spec,
+									Status: v1alpha1.SourceResolverStatus{
+										Status: corev1alpha1.Status{
+											ObservedGeneration: originalGeneration,
+											Conditions: corev1alpha1.Conditions{
+												{
+													Type:   corev1alpha1.ConditionReady,
+													Status: corev1.ConditionTrue,
+												},
+												{
+													Type:   v1alpha1.ActivePolling,
+													Status: corev1.ConditionFalse,
+												},
+											},
+										},
+										Source: v1alpha1.ResolvedSourceConfig{
+											Git: &v1alpha1.ResolvedGitSource{
+												URL:      "https://example.com/something",
+												Revision: "abcdef",
+												Type:     v1alpha1.Commit,
+											},
+										},
+									},
+								},
+							},
+						},
+					})
+
+					require.Equal(t, 0, fakeEnqueuer.EnqueueCallCount())
+				})
+			})
+
+			when("git resolves to unknown", func() {
+				resolvedSource := v1alpha1.ResolvedSourceConfig{
+					Git: &v1alpha1.ResolvedGitSource{
+						URL:      "https://example.com/something",
+						Revision: "abcdef",
+						Type:     v1alpha1.Unknown,
+					},
+				}
+
+				fakeGitResolver.ResolveReturns(resolvedSource, nil)
+				fakeGitResolver.CanResolveReturns(true)
+
+				it("saves unknown when source has not previously resolved", func() {
+					sourceResolver.Generation = 1
+
+					rt.Test(rtesting.TableRow{
+						Key: key,
+						Objects: []runtime.Object{
+							sourceResolver,
+						},
+						WantErr: false,
+						WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+							{
+								Object: &v1alpha1.SourceResolver{
+									ObjectMeta: sourceResolver.ObjectMeta,
+									Spec:       sourceResolver.Spec,
+									Status: v1alpha1.SourceResolverStatus{
+										Status: corev1alpha1.Status{
+											ObservedGeneration: 1,
+											Conditions: corev1alpha1.Conditions{
+												{
+													Type:   corev1alpha1.ConditionReady,
+													Status: corev1.ConditionTrue,
+												},
+												{
+													Type:   v1alpha1.ActivePolling,
+													Status: corev1.ConditionFalse,
+												},
+											},
+										},
+										Source: v1alpha1.ResolvedSourceConfig{
+											Git: &v1alpha1.ResolvedGitSource{
+												URL:      "https://example.com/something",
+												Revision: "abcdef",
+												Type:     v1alpha1.Unknown,
+											},
+										},
+									},
+								},
+							},
+						},
+					})
+				})
+
+				it("ignores unknown when source has been previously resolved", func() {
+					alreadyResolvedSource := v1alpha1.ResolvedSourceConfig{
+						Git: &v1alpha1.ResolvedGitSource{
+							URL:      "https://example.com/something",
+							Revision: "abcdef",
+							Type:     v1alpha1.Commit,
+						},
+					}
+
+					alreadyResolvedSourceResolver := sourceResolver.DeepCopy()
+					alreadyResolvedSourceResolver = resolvedSourceResolver(alreadyResolvedSourceResolver, alreadyResolvedSource)
+					rt.Test(rtesting.TableRow{
+						Key: key,
+						Objects: []runtime.Object{
+							sourceResolver,
+						},
+						WantErr: false,
+					})
+				})
+			})
+		})
+
+		when("a blob based source config", func() {
+			sourceResolver := &v1alpha1.SourceResolver{
+				ObjectMeta: v1.ObjectMeta{
+					Name:       sourceResolverName,
+					Namespace:  namespace,
+					Generation: originalGeneration,
+				},
+				Spec: v1alpha1.SourceResolverSpec{
+					ServiceAccount: serviceAccount,
+					Source: v1alpha1.SourceConfig{
+						Blob: &v1alpha1.Blob{
+							URL: "https://some-blobstore.example.com/some-blob",
+						},
+					},
+				},
+			}
+
+			resolvedSource := v1alpha1.ResolvedSourceConfig{
+				Blob: &v1alpha1.ResolvedBlobSource{
+					URL: "https://some-blobstore.example.com/some-blob",
+				},
+			}
+
+			fakeBlobResolver.ResolveReturns(resolvedSource, nil)
+			fakeBlobResolver.CanResolveReturns(true)
+
+			it("reconciles to ready and not active polling", func() {
+				rt.Test(rtesting.TableRow{
+					Key: key,
+					Objects: []runtime.Object{
+						sourceResolver,
+					},
+					WantErr: false,
+					WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+						{
+							Object: &v1alpha1.SourceResolver{
+								ObjectMeta: sourceResolver.ObjectMeta,
+								Spec:       sourceResolver.Spec,
+								Status: v1alpha1.SourceResolverStatus{
+									Status: corev1alpha1.Status{
+										ObservedGeneration: originalGeneration,
+										Conditions: corev1alpha1.Conditions{
+											{
+												Type:   corev1alpha1.ConditionReady,
+												Status: corev1.ConditionTrue,
+											},
+											{
+												Type:   v1alpha1.ActivePolling,
+												Status: corev1.ConditionFalse,
+											},
+										},
+									},
+									Source: v1alpha1.ResolvedSourceConfig{
+										Blob: &v1alpha1.ResolvedBlobSource{
+											URL: "https://some-blobstore.example.com/some-blob",
+										},
+									},
+								},
+							},
+						},
+					},
+				})
+			})
+		})
+
+		when("a registry based source config", func() {
+			sourceResolver := &v1alpha1.SourceResolver{
+				ObjectMeta: v1.ObjectMeta{
+					Name:       sourceResolverName,
+					Namespace:  namespace,
+					Generation: originalGeneration,
+				},
+				Spec: v1alpha1.SourceResolverSpec{
+					ServiceAccount: serviceAccount,
+					Source: v1alpha1.SourceConfig{
+						Registry: &v1alpha1.Registry{
+							Image: "some-registry.io/some-image@sha256:abcdef123456",
+						},
+					},
+				},
+			}
+
+			resolvedSource := v1alpha1.ResolvedSourceConfig{
+				Registry: &v1alpha1.ResolvedRegistrySource{
+					Image: "some-registry.io/some-image@sha256:abcdef123456",
+				},
+			}
+
+			fakeRegistryResolver.ResolveReturns(resolvedSource, nil)
+			fakeRegistryResolver.CanResolveReturns(true)
+
+			it("reconciles to ready and not active polling", func() {
+				rt.Test(rtesting.TableRow{
+					Key: key,
+					Objects: []runtime.Object{
+						sourceResolver,
+					},
+					WantErr: false,
+					WantStatusUpdates: []clientgotesting.UpdateActionImpl{
+						{
+							Object: &v1alpha1.SourceResolver{
+								ObjectMeta: sourceResolver.ObjectMeta,
+								Spec:       sourceResolver.Spec,
+								Status: v1alpha1.SourceResolverStatus{
+									Status: corev1alpha1.Status{
+										ObservedGeneration: originalGeneration,
+										Conditions: corev1alpha1.Conditions{
+											{
+												Type:   corev1alpha1.ConditionReady,
+												Status: corev1.ConditionTrue,
+											},
+											{
+												Type:   v1alpha1.ActivePolling,
+												Status: corev1.ConditionFalse,
+											},
+										},
+									},
+									Source: v1alpha1.ResolvedSourceConfig{
+										Registry: &v1alpha1.ResolvedRegistrySource{
+											Image: "some-registry.io/some-image@sha256:abcdef123456",
+										},
+									},
+								},
+							},
+						},
+					},
+				})
+			})
+		})
+	})
+}
+
+func resolvedSourceResolver(sourceResolver *v1alpha1.SourceResolver, resolvedSource v1alpha1.ResolvedSourceConfig) *v1alpha1.SourceResolver {
+	sourceResolver.ResolvedSource(resolvedSource)
+	return sourceResolver
+}
