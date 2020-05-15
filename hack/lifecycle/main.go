65f081ae67d4c2abd026a69c6edc09115817037a
Matthew McNew <mmcnew@pivotal.io>
2020-05-15 13:29:52 -0500

    Update lifecycle image version to 0.7.5

--- hack/lifecycle/main.go
+++ hack/lifecycle/main.go
@@ -41,10 +41,10 @@ func main() {
 	flag.Parse()
 
 	image, err := lifecycleImage(
-		"https://github.com/buildpacks/lifecycle/releases/download/v0.7.3/lifecycle-v0.7.3+linux.x86-64.tgz",
+		"https://github.com/buildpacks/lifecycle/releases/download/v0.7.5/lifecycle-v0.7.5+linux.x86-64.tgz",
 		cnb.LifecycleMetadata{
 			LifecycleInfo: cnb.LifecycleInfo{
-				Version: "0.7.3",
+				Version: "0.7.5",
 			},
 			API: cnb.LifecycleAPI{
 				BuildpackVersion: "0.2",
