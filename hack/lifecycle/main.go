74517f54aa558ae9426ebaf4a7d1c7e31c81e28d
Matthew McNew <mmcnew@pivotal.io>
2020-06-03 16:01:08 -0500

    Build lifecycle image with 0.8.0
    Signed-off-by: Matthew McNew <mmcnew@pivotal.io>

--- hack/lifecycle/main.go
+++ hack/lifecycle/main.go
@@ -41,10 +41,10 @@ func main() {
 	flag.Parse()
 
 	image, err := lifecycleImage(
-		"https://github.com/buildpacks/lifecycle/releases/download/v0.7.5/lifecycle-v0.7.5+linux.x86-64.tgz",
+		"https://github.com/buildpacks/lifecycle/releases/download/v0.8.0/lifecycle-v0.8.0+linux.x86-64.tgz",
 		cnb.LifecycleMetadata{
 			LifecycleInfo: cnb.LifecycleInfo{
-				Version: "0.7.5",
+				Version: "0.8.0",
 			},
 			API: cnb.LifecycleAPI{
 				BuildpackVersion: "0.2",
