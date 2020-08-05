b96b3a70fefdb8b9a679a39100b700d5f4a7bb9c
Matthew McNew <mmcnew@pivotal.io>
2020-08-05 13:26:58 -0500

    Allow version and commit to be configured with individually

--- cmd/version.go
+++ cmd/version.go
@@ -1,6 +1,10 @@
 package cmd
 
+import "fmt"
+
 // This is set by CI when creating release binaries
 var (
-	Version = "v0.0.0"
+	Version   = "0.0.0"
+	Commit    = "000"
+	Identifer = fmt.Sprintf("v%s (git sha: %s})", Version, Commit)
 )
