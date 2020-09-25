0b62bbe197864636e4db5fd5d0af5bbaf5cd0f93
Tyler Phelan <tylerp@vmware.com>
2020-09-25 11:11:09 -0400

    Add yaml tags for kpack cli unmarshaling of builder order
    - Useful for enabling builder order yaml config files using gopkg.in/yaml package
    - Run goimports and openapi codegen

--- pkg/apis/build/v1alpha1/buildpack_types.go
+++ pkg/apis/build/v1alpha1/buildpack_types.go
@@ -7,19 +7,19 @@ type Order []OrderEntry
 // +k8s:openapi-gen=true
 type OrderEntry struct {
 	// +listType
-	Group []BuildpackRef `json:"group,omitempty"`
+	Group []BuildpackRef `json:"group,omitempty" yaml:"group,omitempty"`
 }
 
 // +k8s:openapi-gen=true
 type BuildpackRef struct {
-	BuildpackInfo `json:",inline"`
-	Optional      bool `json:"optional,omitempty"`
+	BuildpackInfo `json:",inline" yaml:",inline"`
+	Optional      bool `json:"optional,omitempty" yaml:"optional,omitempty"`
 }
 
 // +k8s:openapi-gen=true
 type BuildpackInfo struct {
-	Id      string `json:"id"`
-	Version string `json:"version,omitempty"`
+	Id      string `json:"id" yaml:"id"`
+	Version string `json:"version,omitempty" yaml:"version,omitempty"`
 }
 
 func (b BuildpackInfo) String() string {
