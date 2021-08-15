37ab1c385d5a492bbf8ba069b6f4187ea37543ef
Sambhav Kothari <skothari44@bloomberg.net>
2021-08-15 10:35:38 +0000

    Add support for project descriptor v0.2
    Signed-off-by: Sambhav Kothari <skothari44@bloomberg.net>

--- pkg/cnb/project_descriptor.go
+++ pkg/cnb/project_descriptor.go
@@ -12,32 +12,58 @@ import (
 
 const fileDescriptorName = "project.toml"
 
-func ProcessProjectDescriptor(appDir, platformDir string, logger *log.Logger) error {
+func parseProjectDescriptor(file string, logger *log.Logger) (build, error) {
+	var dv2 descriptorV2
+	if _, err := toml.DecodeFile(file, &dv2); err != nil {
+		return build{}, err
+	}
+
+	if dv2.Project.SchemaVersion != "" {
+		if dv2.Project.SchemaVersion == "0.2" {
+			// Normalizing the buildpacks table to a common schema
+			dv2.IO.Buildpacks.Buildpacks = dv2.IO.Buildpacks.Group
+			dv2.IO.Buildpacks.Group = nil
+			return dv2.IO.Buildpacks, nil
+		} else {
+			logger.Println(fmt.Sprintf("warning: project descriptor version %s is unsupported and %s will be ignored", dv2.Project.SchemaVersion, file))
+			return build{}, nil
+		}
+	}
 	var d descriptor
+	if _, err := toml.DecodeFile(file, &d); err != nil {
+		return build{}, err
+	}
+	// Removing groups from v1 descriptor if it exists
+	d.Build.Group = nil
+	return d.Build, nil
+}
+
+func ProcessProjectDescriptor(appDir, platformDir string, logger *log.Logger) error {
+
 	file := filepath.Join(appDir, fileDescriptorName)
 	if _, err := os.Stat(file); os.IsNotExist(err) {
 		return nil
 	} else if err != nil {
 		return fmt.Errorf("unable to determine if project descriptor file exists: %w", err)
 	}
-	_, err := toml.DecodeFile(file, &d)
+	d, err := parseProjectDescriptor(file, logger)
 	if err != nil {
 		return err
 	}
-	if d.Build.Buildpacks != nil {
+	if d.Buildpacks != nil {
 		logger.Println("info: buildpacks provided in project descriptor file will be ignored")
 	}
 
-	if d.Build.Builder != "" {
+	if d.Builder != "" {
 		logger.Println("info: builder provided in project descriptor file will be ignored")
 	}
 	if err := processFiles(appDir, d); err != nil {
 		return err
 	}
-	return serializeEnvVars(d.Build.Env, platformDir)
+	return serializeEnvVars(d.Env, platformDir)
 }
 
-func processFiles(appDir string, d descriptor) error {
+func processFiles(appDir string, d build) error {
 	fileFilter, err := getFileFilter(d)
 	if err != nil {
 		return err
@@ -66,19 +92,19 @@ func processFiles(appDir string, d descriptor) error {
 	})
 }
 
-func getFileFilter(d descriptor) (func(string) bool, error) {
-	if d.Build.Exclude != nil && d.Build.Include != nil {
+func getFileFilter(d build) (func(string) bool, error) {
+	if d.Exclude != nil && d.Include != nil {
 		return nil, fmt.Errorf("%s: cannot have both include and exclude defined", fileDescriptorName)
 	}
 
-	if len(d.Build.Exclude) > 0 {
-		excludes := ignore.CompileIgnoreLines(d.Build.Exclude...)
+	if len(d.Exclude) > 0 {
+		excludes := ignore.CompileIgnoreLines(d.Exclude...)
 		return func(fileName string) bool {
 			return !excludes.MatchesPath(fileName)
 		}, nil
 	}
-	if len(d.Build.Include) > 0 {
-		includes := ignore.CompileIgnoreLines(d.Build.Include...)
+	if len(d.Include) > 0 {
+		includes := ignore.CompileIgnoreLines(d.Include...)
 		return includes.MatchesPath, nil
 	}
 
@@ -95,10 +121,24 @@ type build struct {
 	Include    []string      `toml:"include"`
 	Exclude    []string      `toml:"exclude"`
 	Buildpacks []buildpack   `toml:"buildpacks"`
+	Group      []buildpack   `toml:"group"`
 	Builder    string        `toml:"builder"`
 	Env        []envVariable `toml:"env"`
 }
 
 type descriptor struct {
 	Build build `toml:"build"`
+}
+
+type descriptorV2 struct {
+	Project project `toml:"_"`
+	IO      ioTable `toml:"io"`
+}
+
+type project struct {
+	SchemaVersion string `toml:"schema-version"`
+}
+
+type ioTable struct {
+	Buildpacks build `toml:"buildpacks"`
 }
--- pkg/cnb/project_descriptor_test.go
+++ pkg/cnb/project_descriptor_test.go
@@ -2,6 +2,7 @@ package cnb_test
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -40,6 +41,19 @@ func testProcessProjectDescriptor(t *testing.T, when spec.G, it spec.S) {
 	it.After(func() {
 		os.RemoveAll(appDir)
 		os.RemoveAll(platformDir)
+	})
+
+	when("unsupported project descriptor version", func() {
+		it.Before(func() {
+			ioutil.WriteFile(projectToml, []byte(`
+[_]
+schema-version = "0.3"
+			`), 0644)
+		})
+		it("logs a warning the project descriptor will be ignored", func() {
+			assert.Nil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
+			assert.Equal(t, fmt.Sprintf("warning: project descriptor version 0.3 is unsupported and %s will be ignored\n", projectToml), buf.String())
+		})
 	})
 
 	when("#process", func() {
@@ -204,6 +218,224 @@ uri = "check-this-out.com"
 			it("logs a warning that the buildpacks will be ignored", func() {
 				assert.Nil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
 				assert.Equal(t, "info: buildpacks provided in project descriptor file will be ignored\n", buf.String())
+			})
+		})
+
+		when("the descriptor has groups", func() {
+			it.Before(func() {
+				ioutil.WriteFile(projectToml, []byte(`
+[[build.groups]]
+id = "cool-buildpack"
+version = "v4.2"
+uri = "check-this-out.com"
+				`), 0644)
+			})
+			it("it ignores the table", func() {
+				assert.Nil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
+				assert.Equal(t, "", buf.String())
+			})
+		})
+
+	})
+	when("#processv2", func() {
+		when("the descriptor has build env vars", func() {
+			it.Before(func() {
+				ioutil.WriteFile(projectToml, []byte(`
+[_]
+schema-version = "0.2"
+
+[[io.buildpacks.env]]
+name = "keyA"
+value = "valueA"
+
+[[io.buildpacks.env]]
+name = "keyB"
+value = "valueB"
+
+[[io.buildpacks.env]]
+name = "keyC"
+value = "valueC"
+
+# check that later keys override previous ones
+[[io.buildpacks.env]]
+name = "keyC"
+value = "valueAnotherC"
+
+				`), 0644)
+			})
+			it("writes all env var files to the platform dir", func() {
+				assert.Nil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
+				checkEnvVar(t, platformDir, "keyA", "valueA")
+				checkEnvVar(t, platformDir, "keyB", "valueB")
+				checkEnvVar(t, platformDir, "keyC", "valueAnotherC")
+			})
+		})
+
+		when("the descriptor has includes and excludes", func() {
+			it.Before(func() {
+				var err error
+				// Create test directories and files:
+				//
+				// ├── cookie.jar
+				// ├── other-cookie.jar
+				// ├── nested-cookie.jar
+				// ├── nested
+				// │   └── nested-cookie.jar
+				// ├── secrets
+				// │   ├── api_keys.json
+				// |   |── user_token
+				// ├── media
+				// │   ├── mountain.jpg
+				// │   └── person.png
+				// └── test.sh
+
+				err = os.Mkdir(filepath.Join(appDir, "secrets"), 0755)
+				assert.Nil(t, err)
+				err = ioutil.WriteFile(filepath.Join(appDir, "secrets", "api_keys.json"), []byte("{}"), 0755)
+				assert.Nil(t, err)
+				err = ioutil.WriteFile(filepath.Join(appDir, "secrets", "user_token"), []byte("token"), 0755)
+				assert.Nil(t, err)
+
+				err = os.Mkdir(filepath.Join(appDir, "nested"), 0755)
+				assert.Nil(t, err)
+				err = ioutil.WriteFile(filepath.Join(appDir, "nested", "nested-cookie.jar"), []byte("chocolate chip"), 0755)
+				assert.Nil(t, err)
+
+				err = ioutil.WriteFile(filepath.Join(appDir, "other-cookie.jar"), []byte("chocolate chip"), 0755)
+				assert.Nil(t, err)
+
+				err = ioutil.WriteFile(filepath.Join(appDir, "nested-cookie.jar"), []byte("chocolate chip"), 0755)
+				assert.Nil(t, err)
+
+				err = os.Mkdir(filepath.Join(appDir, "media"), 0755)
+				assert.Nil(t, err)
+				err = ioutil.WriteFile(filepath.Join(appDir, "media", "mountain.jpg"), []byte("fake image bytes"), 0755)
+				assert.Nil(t, err)
+				err = ioutil.WriteFile(filepath.Join(appDir, "media", "person.png"), []byte("fake image bytes"), 0755)
+				assert.Nil(t, err)
+
+				err = ioutil.WriteFile(filepath.Join(appDir, "cookie.jar"), []byte("chocolate chip"), 0755)
+				assert.Nil(t, err)
+				err = ioutil.WriteFile(filepath.Join(appDir, "test.sh"), []byte("echo test"), 0755)
+				assert.Nil(t, err)
+			})
+
+			when("it has excludes", func() {
+				it.Before(func() {
+					ioutil.WriteFile(projectToml, []byte(`
+[_]
+schema-version = "0.2"
+
+[io.buildpacks]
+exclude = ["*.sh", "secrets/", "media/metadata", "/other-cookie.jar" ,"/nested-cookie.jar"]					
+					`), 0644)
+				})
+				it("removes the excluded files", func() {
+					assert.Nil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
+					assert.NoFileExists(t, filepath.Join(appDir, "api_keys.json"))
+					assert.NoFileExists(t, filepath.Join(appDir, "user_token"))
+					assert.NoFileExists(t, filepath.Join(appDir, "test.sh"))
+					assert.NoFileExists(t, filepath.Join(appDir, "other-cookie.jar"))
+					assert.NoFileExists(t, filepath.Join(appDir, "nested-cookie.jar"))
+					assert.FileExists(t, filepath.Join(appDir, "cookie.jar"))
+					assert.FileExists(t, filepath.Join(appDir, "nested", "nested-cookie.jar"))
+					assert.FileExists(t, filepath.Join(appDir, "media", "mountain.jpg"))
+					assert.FileExists(t, filepath.Join(appDir, "media", "person.png"))
+				})
+			})
+
+			when("it has includes", func() {
+				it.Before(func() {
+					ioutil.WriteFile(projectToml, []byte(`
+[_]
+schema-version = "0.2"
+
+[io.buildpacks]
+include = [ "*.jar", "media/mountain.jpg", "/media/person.png", ]
+					`), 0644)
+				})
+
+				it("keeps only the included files", func() {
+					assert.Nil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
+					assert.NoFileExists(t, filepath.Join(appDir, "api_keys.json"))
+					assert.NoFileExists(t, filepath.Join(appDir, "user_token"))
+					assert.NoFileExists(t, filepath.Join(appDir, "test.sh"))
+					assert.FileExists(t, filepath.Join(appDir, "other-cookie.jar"))
+					assert.FileExists(t, filepath.Join(appDir, "nested-cookie.jar"))
+					assert.FileExists(t, filepath.Join(appDir, "cookie.jar"))
+					assert.FileExists(t, filepath.Join(appDir, "nested", "nested-cookie.jar"))
+					assert.FileExists(t, filepath.Join(appDir, "media", "mountain.jpg"))
+					assert.FileExists(t, filepath.Join(appDir, "media", "person.png"))
+				})
+			})
+
+			when("it has both excludes and includes", func() {
+				it.Before(func() {
+					ioutil.WriteFile(projectToml, []byte(`
+[_]
+schema-version = "0.2"
+
+[io.buildpacks]
+include = [ "test", ]
+exclude = ["test", ]
+					`), 0644)
+				})
+				it("throws an error", func() {
+					assert.NotNil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
+				})
+
+			})
+		})
+		when("the descriptor has builder", func() {
+			it.Before(func() {
+				ioutil.WriteFile(projectToml, []byte(`
+[_]
+schema-version = "0.2"
+
+[io.buildpacks]
+builder = "my-super-cool-builder"
+
+				`), 0644)
+			})
+			it("logs a warning that the builder will be ignored", func() {
+				assert.Nil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
+				assert.Equal(t, "info: builder provided in project descriptor file will be ignored\n", buf.String())
+			})
+		})
+
+		when("the descriptor has buildpack groups", func() {
+			it.Before(func() {
+				ioutil.WriteFile(projectToml, []byte(`
+[_]
+schema-version = "0.2"
+
+[[io.buildpacks.group]]
+id = "cool-buildpack"
+version = "v4.2"
+uri = "check-this-out.com"
+				`), 0644)
+			})
+			it("logs a warning that the buildpacks will be ignored", func() {
+				assert.Nil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
+				assert.Equal(t, "info: buildpacks provided in project descriptor file will be ignored\n", buf.String())
+			})
+		})
+
+		when("the descriptor has buildpacks using the buildpacks key", func() {
+			it.Before(func() {
+				ioutil.WriteFile(projectToml, []byte(`
+[_]
+schema-version = "0.2"
+
+[[io.buildpacks.buildpacks]]
+id = "cool-buildpack"
+version = "v4.2"
+uri = "check-this-out.com"
+				`), 0644)
+			})
+			it("ignores the buildpacks key", func() {
+				assert.Nil(t, cnb.ProcessProjectDescriptor(appDir, platformDir, logger))
+				assert.Equal(t, "", buf.String())
 			})
 		})
 
