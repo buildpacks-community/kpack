2ae7f7dae8631bc872c4236e33fb0b7bc756da99
Tyler Phelan <tylerp@vmware.com>
2021-08-30 12:21:14 -0400

    Update tests for conversions
    - No longer test by converting to and back due to new fields that can be lost
    - Add test to call out fields that cannot be converted back to v1alpha1

--- pkg/apis/build/v1alpha2/build_conversion_test.go
+++ pkg/apis/build/v1alpha2/build_conversion_test.go
@@ -20,7 +20,7 @@ func TestBuildConversion(t *testing.T) {
 
 func testBuildConversion(t *testing.T, when spec.G, it spec.S) {
 	when("converting to and from v1alpha1", func() {
-		build := &Build{
+		v1alpha2Build := &Build{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "my-build",
 			},
@@ -80,9 +80,72 @@ func testBuildConversion(t *testing.T, when spec.G, it spec.S) {
 				StepsCompleted: []string{},
 			},
 		}
+		v1alpha1Build := &v1alpha1.Build{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "my-build",
+			},
+			Spec: v1alpha1.BuildSpec{
+				Tags: []string{"tag"},
+				Builder: corev1alpha1.BuildBuilderSpec{
+					Image: "my-builder=image",
+					ImagePullSecrets: []corev1.LocalObjectReference{{
+						Name: "secret",
+					}},
+				},
+				ServiceAccount: "default",
+				CacheName:      "some-claim-name",
+				Bindings:       nil,
+				Env: []corev1.EnvVar{{
+					Name:  "some-var",
+					Value: "some-val",
+				}},
+				Resources: corev1.ResourceRequirements{
+					Limits: map[corev1.ResourceName]resource.Quantity{
+						"some-name": resource.MustParse("5M"),
+					},
+					Requests: map[corev1.ResourceName]resource.Quantity{
+						"some-name": resource.MustParse("5M"),
+					},
+				},
+				LastBuild: &v1alpha1.LastBuild{
+					Image:   "last-image",
+					StackId: "my-stack",
+				},
+				Notary: &corev1alpha1.NotaryConfig{
+					V1: &corev1alpha1.NotaryV1Config{
+						URL: "notary.com",
+						SecretRef: corev1alpha1.NotarySecretRef{
+							Name: "notary",
+						},
+					},
+				},
+			},
+			Status: v1alpha1.BuildStatus{
+				Status: corev1alpha1.Status{
+					ObservedGeneration: 0,
+					Conditions:         nil,
+				},
+				BuildMetadata: corev1alpha1.BuildpackMetadataList{},
+				Stack: corev1alpha1.BuildStack{
+					RunImage: "some-run",
+					ID:       "some-id",
+				},
+				LatestImage:    "some-latest",
+				PodName:        "some-pod",
+				StepStates:     []corev1.ContainerState{},
+				StepsCompleted: []string{},
+			},
+		}
 
-		it("does not have any data loss with git source", func() {
-			build.Spec.Source = corev1alpha1.SourceConfig{
+		it("converts with git source", func() {
+			v1alpha2Build.Spec.Source = corev1alpha1.SourceConfig{
+				Git: &corev1alpha1.Git{
+					URL:      "my-repo.com",
+					Revision: "main",
+				},
+				SubPath: "my-sub-path",
+			}
+			v1alpha1Build.Spec.Source = corev1alpha1.SourceConfig{
 				Git: &corev1alpha1.Git{
 					URL:      "my-repo.com",
 					Revision: "main",
@@ -90,18 +153,30 @@ func testBuildConversion(t *testing.T, when spec.G, it spec.S) {
 				SubPath: "my-sub-path",
 			}
 
-			v1alpha1Build := &v1alpha1.Build{}
-			err := build.DeepCopy().ConvertTo(context.TODO(), v1alpha1Build)
+			testV1alpha1Build := &v1alpha1.Build{}
+			err := v1alpha2Build.ConvertTo(context.TODO(), testV1alpha1Build)
 			require.NoError(t, err)
+			require.Equal(t, testV1alpha1Build, v1alpha1Build)
 
-			convertedBackBuild := &Build{}
-			err = convertedBackBuild.ConvertFrom(context.TODO(), v1alpha1Build)
+			testV1alpha2Build := &Build{}
+			err = testV1alpha2Build.ConvertFrom(context.TODO(), v1alpha1Build)
 			require.NoError(t, err)
-
-			require.Equal(t, build, convertedBackBuild)
+			require.Equal(t, testV1alpha2Build, v1alpha2Build)
 		})
-		it("does not have any data loss with registry source", func() {
-			build.Spec.Source = corev1alpha1.SourceConfig{
+
+		it("converts with registry source", func() {
+			v1alpha2Build.Spec.Source = corev1alpha1.SourceConfig{
+				Registry: &corev1alpha1.Registry{
+					Image: "my-registry.com/image",
+					ImagePullSecrets: []corev1.LocalObjectReference{
+						{
+							Name: "my-image-secret",
+						},
+					},
+				},
+				SubPath: "my-sub-path",
+			}
+			v1alpha1Build.Spec.Source = corev1alpha1.SourceConfig{
 				Registry: &corev1alpha1.Registry{
 					Image: "my-registry.com/image",
 					ImagePullSecrets: []corev1.LocalObjectReference{
@@ -113,48 +188,72 @@ func testBuildConversion(t *testing.T, when spec.G, it spec.S) {
 				SubPath: "my-sub-path",
 			}
 
-			v1alpha1Build := &v1alpha1.Build{}
-			err := build.DeepCopy().ConvertTo(context.TODO(), v1alpha1Build)
+			testV1alpha1Build := &v1alpha1.Build{}
+			err := v1alpha2Build.ConvertTo(context.TODO(), testV1alpha1Build)
 			require.NoError(t, err)
+			require.Equal(t, testV1alpha1Build, v1alpha1Build)
 
-			convertedBackBuild := &Build{}
-			err = convertedBackBuild.ConvertFrom(context.TODO(), v1alpha1Build)
+			testV1alpha2Build := &Build{}
+			err = testV1alpha2Build.ConvertFrom(context.TODO(), v1alpha1Build)
 			require.NoError(t, err)
-
-			require.Equal(t, build, convertedBackBuild)
+			require.Equal(t, testV1alpha2Build, v1alpha2Build)
 		})
 
-		it("does not have any data loss with blob source", func() {
-			build.Spec.Source = corev1alpha1.SourceConfig{
+		it("converts with blob source", func() {
+			v1alpha2Build.Spec.Source = corev1alpha1.SourceConfig{
+				Blob: &corev1alpha1.Blob{
+					URL: "my-blob.com",
+				},
+				SubPath: "my-sub-path",
+			}
+			v1alpha1Build.Spec.Source = corev1alpha1.SourceConfig{
 				Blob: &corev1alpha1.Blob{
 					URL: "my-blob.com",
 				},
 				SubPath: "my-sub-path",
 			}
 
-			v1alpha1Build := &v1alpha1.Build{}
-			err := build.DeepCopy().ConvertTo(context.TODO(), v1alpha1Build)
+			testV1alpha1Build := &v1alpha1.Build{}
+			err := v1alpha2Build.ConvertTo(context.TODO(), testV1alpha1Build)
 			require.NoError(t, err)
+			require.Equal(t, testV1alpha1Build, v1alpha1Build)
 
-			convertedBackBuild := &Build{}
-			err = convertedBackBuild.ConvertFrom(context.TODO(), v1alpha1Build)
+			testV1alpha2Build := &Build{}
+			err = testV1alpha2Build.ConvertFrom(context.TODO(), v1alpha1Build)
 			require.NoError(t, err)
+			require.Equal(t, testV1alpha2Build, v1alpha2Build)
+		})
 
-			require.Equal(t, build, convertedBackBuild)
+		it("can't convert new fields to v1alpha1", func() {
+			v1alpha2Build.Spec.ProjectDescriptorPath = "some-path"
+			v1alpha2Build.Spec.Cache.Registry = &RegistryCache{
+				Tag: "some-cache-tag",
+			}
+			v1alpha2Build.Spec.LastBuild.Cache = BuildCache{
+				Image: "some-cache-image",
+			}
+			v1alpha2Build.Status.LatestCacheImage = "some-latest-cache"
+
+			testV1alpha1Build := &v1alpha1.Build{}
+			err := v1alpha2Build.ConvertTo(context.TODO(), testV1alpha1Build)
+			require.NoError(t, err)
+			require.Equal(t, testV1alpha1Build, v1alpha1Build)
 		})
 
 		it("handles null values", func() {
-			build.Spec.LastBuild = nil
-			build.Spec.Notary = nil
-			v1alpha1Build := &v1alpha1.Build{}
-			err := build.DeepCopy().ConvertTo(context.TODO(), v1alpha1Build)
+			v1alpha2Build.Spec.LastBuild = nil
+			v1alpha2Build.Spec.Notary = nil
+			v1alpha1Build.Spec.LastBuild = nil
+			v1alpha1Build.Spec.Notary = nil
+			testV1alpha1Build := &v1alpha1.Build{}
+			err := v1alpha2Build.ConvertTo(context.TODO(), testV1alpha1Build)
 			require.NoError(t, err)
+			require.Equal(t, testV1alpha1Build, v1alpha1Build)
 
-			convertedBackBuild := &Build{}
-			err = convertedBackBuild.ConvertFrom(context.TODO(), v1alpha1Build)
+			testV1alpha2Build := &Build{}
+			err = testV1alpha2Build.ConvertFrom(context.TODO(), v1alpha1Build)
 			require.NoError(t, err)
-
-			require.Equal(t, build, convertedBackBuild)
+			require.Equal(t, testV1alpha2Build, v1alpha2Build)
 		})
 	})
 }
