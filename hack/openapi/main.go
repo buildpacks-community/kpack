8c3d874eeaf18403cc1cc65cd1037087e2106da1
Tom Kennedy <ktom@vmware.com>
2021-09-24 17:49:51 -0400

    update openapi

--- hack/openapi/main.go
+++ hack/openapi/main.go
@@ -1068,6 +1068,63 @@ const k8sOpenAPIDefinitions = `{
       }
     },
     "type": "object"
+  },
+  "io.k8s.api.core.v1.NodeAffinity": {
+    "description": "Node affinity is a group of node affinity scheduling rules.",
+    "properties": {
+      "preferredDuringSchedulingIgnoredDuringExecution": {
+        "description": "The scheduler will prefer to schedule pods to nodes that satisfy the affinity expressions specified by this field, but it may choose a node that violates one or more of the expressions. The node that is most preferred is the one with the greatest sum of weights, i.e. for each node that meets all of the scheduling requirements (resource request, requiredDuringScheduling affinity expressions, etc.), compute a sum by iterating through the elements of this field and adding \"weight\" to the sum if the node matches the corresponding matchExpressions; the node(s) with the highest sum are the most preferred.",
+        "items": {
+          "$ref": "#/definitions/io.k8s.api.core.v1.PreferredSchedulingTerm"
+        },
+        "type": "array"
+      },
+      "requiredDuringSchedulingIgnoredDuringExecution": {
+        "$ref": "#/definitions/io.k8s.api.core.v1.NodeSelector",
+        "description": "If the affinity requirements specified by this field are not met at scheduling time, the pod will not be scheduled onto the node. If the affinity requirements specified by this field cease to be met at some point during pod execution (e.g. due to an update), the system may or may not try to eventually evict the pod from its node."
+      }
+    },
+    "type": "object"
+  },
+  "io.k8s.api.core.v1.PodAntiAffinity": {
+    "description": "Pod anti affinity is a group of inter pod anti affinity scheduling rules.",
+    "properties": {
+      "preferredDuringSchedulingIgnoredDuringExecution": {
+        "description": "The scheduler will prefer to schedule pods to nodes that satisfy the anti-affinity expressions specified by this field, but it may choose a node that violates one or more of the expressions. The node that is most preferred is the one with the greatest sum of weights, i.e. for each node that meets all of the scheduling requirements (resource request, requiredDuringScheduling anti-affinity expressions, etc.), compute a sum by iterating through the elements of this field and adding \"weight\" to the sum if the node has pods which matches the corresponding podAffinityTerm; the node(s) with the highest sum are the most preferred.",
+        "items": {
+          "$ref": "#/definitions/io.k8s.api.core.v1.WeightedPodAffinityTerm"
+        },
+        "type": "array"
+      },
+      "requiredDuringSchedulingIgnoredDuringExecution": {
+        "description": "If the anti-affinity requirements specified by this field are not met at scheduling time, the pod will not be scheduled onto the node. If the anti-affinity requirements specified by this field cease to be met at some point during pod execution (e.g. due to a pod label update), the system may or may not try to eventually evict the pod from its node. When there are multiple elements, the lists of nodes corresponding to each podAffinityTerm are intersected, i.e. all terms must be satisfied.",
+        "items": {
+          "$ref": "#/definitions/io.k8s.api.core.v1.PodAffinityTerm"
+        },
+        "type": "array"
+      }
+    },
+    "type": "object"
+  },
+  "io.k8s.api.core.v1.PodAffinity": {
+    "description": "Pod affinity is a group of inter pod affinity scheduling rules.",
+    "properties": {
+      "preferredDuringSchedulingIgnoredDuringExecution": {
+        "description": "The scheduler will prefer to schedule pods to nodes that satisfy the affinity expressions specified by this field, but it may choose a node that violates one or more of the expressions. The node that is most preferred is the one with the greatest sum of weights, i.e. for each node that meets all of the scheduling requirements (resource request, requiredDuringScheduling affinity expressions, etc.), compute a sum by iterating through the elements of this field and adding \"weight\" to the sum if the node has pods which matches the corresponding podAffinityTerm; the node(s) with the highest sum are the most preferred.",
+        "items": {
+          "$ref": "#/definitions/io.k8s.api.core.v1.WeightedPodAffinityTerm"
+        },
+        "type": "array"
+      },
+      "requiredDuringSchedulingIgnoredDuringExecution": {
+        "description": "If the affinity requirements specified by this field are not met at scheduling time, the pod will not be scheduled onto the node. If the affinity requirements specified by this field cease to be met at some point during pod execution (e.g. due to a pod label update), the system may or may not try to eventually evict the pod from its node. When there are multiple elements, the lists of nodes corresponding to each podAffinityTerm are intersected, i.e. all terms must be satisfied.",
+        "items": {
+          "$ref": "#/definitions/io.k8s.api.core.v1.PodAffinityTerm"
+        },
+        "type": "array"
+      }
+    },
+    "type": "object"
   }
 }`
 
