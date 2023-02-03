d8153bee0fc1f81f3993b03ea68a72911b24cf32
Bohan Chen <bohanc@vmware.com>
2023-02-03 10:50:45 -0500

    add stricter builder validation
    Signed-off-by: Bohan Chen <bohanc@vmware.com>

--- pkg/apis/build/v1alpha2/builder_validation.go
+++ pkg/apis/build/v1alpha2/builder_validation.go
@@ -2,11 +2,13 @@ package v1alpha2
 
 import (
 	"context"
+	"strings"
 
 	v1 "k8s.io/api/core/v1"
 
 	"knative.dev/pkg/apis"
 
+	"github.com/pivotal/kpack/pkg/apis/core/v1alpha1"
 	"github.com/pivotal/kpack/pkg/apis/validate"
 )
 
@@ -29,6 +31,7 @@ func (cb *Builder) Validate(ctx context.Context) *apis.FieldError {
 func (s *BuilderSpec) Validate(ctx context.Context) *apis.FieldError {
 	return validate.Tag(s.Tag).
 		Also(validateStack(s.Stack).ViaField("stack")).
+		Also(validateStore(s.Store).ViaField("store")).
 		Also(validateOrder(s.Order).ViaField("order"))
 }
 
@@ -51,22 +54,16 @@ func validateStack(stack v1.ObjectReference) *apis.FieldError {
 }
 
 func validateStore(store v1.ObjectReference) *apis.FieldError {
-	if store.Name == "" {
-		return apis.ErrMissingField("name")
-	}
-
-	switch store.Kind {
-	case ClusterStoreKind:
+	if store.Name == "" && store.Kind == "" {
 		return nil
-	default:
-		return apis.ErrInvalidValue(store.Kind, "kind")
 	}
+	return validateObjectRef(store, []string{ClusterStoreKind})
 }
 
 func validateOrder(order []BuilderOrderEntry) *apis.FieldError {
 	var errs *apis.FieldError
 	for i, s := range order {
-		errs.Also(validateGroup(s).ViaIndex(i))
+		errs = errs.Also(validateGroup(s).ViaIndex(i))
 	}
 	return errs
 }
@@ -74,11 +71,42 @@ func validateOrder(order []BuilderOrderEntry) *apis.FieldError {
 func validateGroup(group BuilderOrderEntry) *apis.FieldError {
 	var errs *apis.FieldError
 	for i, s := range group.Group {
-		errs.Also(validateBuildpackRef(s).ViaIndex(i).ViaField("group"))
+		errs = errs.Also(validateBuildpackRef(s).ViaIndex(i).ViaField("group"))
 	}
 	return errs
 }
 
 func validateBuildpackRef(ref BuilderBuildpackRef) *apis.FieldError {
-	return nil
+	var errs *apis.FieldError
+	if ref.Name != "" || ref.Kind != "" {
+		errs = errs.Also(validateObjectRef(ref.ObjectReference, []string{BuildpackKind, ClusterBuildpackKind}))
+	}
+
+	switch {
+	case ref.Image != "":
+		errs = errs.Also(validate.Image(ref.Image)).
+			Also(apis.CheckDisallowedFields(ref.BuildpackInfo, v1alpha1.BuildpackInfo{})).
+			Also(apis.CheckDisallowedFields(ref.ObjectReference, v1.ObjectReference{}))
+	case ref.Id != "" || ref.Name != "" || ref.Kind != "":
+		if ref.Image != "" {
+			errs = errs.Also(apis.ErrDisallowedFields("image"))
+		}
+	default:
+		errs = errs.Also(apis.ErrMissingOneOf("image", "id", "name + kind"))
+	}
+	return errs
+}
+
+func validateObjectRef(ref v1.ObjectReference, kinds []string) *apis.FieldError {
+	var errs *apis.FieldError
+	if ref.Name == "" {
+		errs = errs.Also(apis.ErrMissingField("name"))
+	}
+
+	for _, k := range kinds {
+		if ref.Kind == k {
+			return nil
+		}
+	}
+	return errs.Also(apis.ErrInvalidValue(ref.Kind, "kind", "must be one of "+strings.Join(kinds, ", ")))
 }
