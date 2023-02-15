e389facec59aeef0fd8e3a8073bc553787314dc3
Tom Kennedy <10608054+tomkennedy513@users.noreply.github.com>
2023-02-15 14:54:06 -0500

    Bump Lifecycle to 0.16.0

--- hack/lifecycle/main.go
+++ hack/lifecycle/main.go
@@ -33,7 +33,7 @@ import (
 const (
 	lifecycleMetadataLabel = "io.buildpacks.lifecycle.metadata"
 	lifecycleLocation      = "/cnb/lifecycle/"
-	lifecycleVersion       = "0.14.3"
+	lifecycleVersion       = "0.16.0"
 )
 
 var (
