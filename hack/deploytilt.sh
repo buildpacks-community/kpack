b9e77c3ba2ea65c6b57d68eade09ed49711a53f3
Shane Lattanzio <slattanzio@pivotal.io>
2023-10-05 11:37:18 -0400

    Automatically comment out non image config map

--- hack/deploytilt.sh
+++ hack/deploytilt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i -e 's/^/#/' config/config-logging.yaml
+
 export KPACK_lifecycle__image="gcr.io/cf-build-service-public/kpack/lifecycle"
 
 ytt -f config/ --data-values-env-yaml KPACK
