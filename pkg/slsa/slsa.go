67adab8a55a8507dc4fa58ac1e8cdd961c689e53
Bohan Chen <bohanc@vmware.com>
2023-12-05 17:06:11 -0500

    add support for basic slsa statements
    todo:
    - extract source from app image labels
    - record builder image
    
    Signed-off-by: Bohan Chen <bohanc@vmware.com>

---
+++ pkg/slsa/slsa.go
@@ -0,0 +1,231 @@
+package slsa
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/google/go-containerregistry/pkg/authn"
+	intoto "github.com/in-toto/in-toto-golang/in_toto"
+	slsacommon "github.com/in-toto/in-toto-golang/in_toto/slsa_provenance/common"
+	slsav1 "github.com/in-toto/in-toto-golang/in_toto/slsa_provenance/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	buildv1alpha2 "github.com/pivotal/kpack/pkg/apis/build/v1alpha2"
+	"github.com/pivotal/kpack/pkg/cnb"
+	"github.com/pivotal/kpack/pkg/config"
+)
+
+const (
+	SignedBuildID   = "https://kpack.io/signed-build"
+	UnsignedBuildID = "https://kpack.io/unsigned-build"
+)
+
+type LifecycleProvider interface {
+	Metadata() (cnb.LifecycleMetadata, error)
+}
+
+type Attester struct {
+	Version string
+
+	LifecycleProvider LifecycleProvider
+
+	Images   config.Images
+	Features config.FeatureFlags
+	Config   config.Config
+}
+
+func (a *Attester) GenerateStatement(build *buildv1alpha2.Build, pod *corev1.Pod, builderImageKeychain, appImageKeychain authn.Keychain, depFns ...BuilderDependencyFn) (intoto.Statement, error) {
+	image := strings.Split(build.Status.LatestImage, "@")[0]
+	digest := strings.Split(build.Status.LatestImage, ":")[1]
+
+	start, stop, err := getStartStopTime(pod)
+	if err != nil {
+		return intoto.Statement{}, fmt.Errorf("parsing start/stop time: %v", err)
+	}
+
+	lifecycle, err := a.LifecycleProvider.Metadata()
+	if err != nil {
+		return intoto.Statement{}, fmt.Errorf("reading lifecycle metadata: %v", err)
+	}
+
+	builderDeps := make([]slsav1.ResourceDescriptor, 0)
+	for i, fn := range depFns {
+		dep, err := fn()
+		if err != nil {
+			return intoto.Statement{}, fmt.Errorf("fetching builder dependency #%v: %v", i, err)
+		}
+
+		builderDeps = append(builderDeps, dep)
+	}
+
+	pred := slsav1.ProvenancePredicate{
+		BuildDefinition: slsav1.ProvenanceBuildDefinition{
+			BuildType:          getBuildType(a.Version),
+			ExternalParameters: build.Spec,
+			InternalParameters: a.internalParamsFor(build),
+		},
+		RunDetails: slsav1.ProvenanceRunDetails{
+			Builder: slsav1.Builder{
+				ID: "",
+				Version: map[string]string{
+					"kpack":     a.Version,
+					"lifecycle": lifecycle.Version,
+				},
+				BuilderDependencies: builderDeps,
+			},
+			BuildMetadata: slsav1.BuildMetadata{
+				InvocationID: getInvocationId(build, pod),
+				StartedOn:    start,
+				FinishedOn:   stop,
+			},
+			Byproducts: []slsav1.ResourceDescriptor{},
+		},
+	}
+
+	return intoto.Statement{
+		StatementHeader: intoto.StatementHeader{
+			Type:          intoto.StatementInTotoV01,
+			PredicateType: slsav1.PredicateSLSAProvenance,
+			Subject: []intoto.Subject{
+				{
+					Name: image,
+					Digest: slsacommon.DigestSet{
+						"sha256": digest,
+					},
+				},
+			},
+		},
+		Predicate: pred,
+	}, nil
+}
+
+type internalParams struct {
+	BuilderImage string `json:"builderImage"`
+
+	config.Config
+	config.Images
+	config.FeatureFlags
+}
+
+func (a *Attester) internalParamsFor(build *buildv1alpha2.Build) internalParams {
+	return internalParams{
+		BuilderImage: build.Spec.Builder.Image,
+
+		Config:       a.Config,
+		FeatureFlags: a.Features,
+		Images:       a.Images,
+	}
+}
+
+func getInvocationId(build *buildv1alpha2.Build, pod *corev1.Pod) string {
+	return fmt.Sprintf("https://kpack.io/%v/%v/%v@%v", build.Namespace, build.Name, pod.Name, pod.Spec.NodeName)
+}
+
+func getBuildType(version string) string {
+	return fmt.Sprintf("https://github.com/buildpacks-community/kpack/blob/v%v/docs/slsa.md", version)
+}
+
+func getStartStopTime(pod *corev1.Pod) (*time.Time, *time.Time, error) {
+	var (
+		start *time.Time
+		stop  *time.Time
+	)
+
+	for _, c := range append(pod.Status.InitContainerStatuses, pod.Status.ContainerStatuses...) {
+		if c.Name == buildv1alpha2.PrepareContainerName {
+			if c.State.Terminated != nil && c.State.Terminated.ExitCode == 0 {
+				start = &c.State.Terminated.StartedAt.Time
+			} else {
+				return nil, nil, fmt.Errorf("prepare not finished yet")
+			}
+		}
+
+		if c.Name == buildv1alpha2.CompletionContainerName {
+			if c.State.Terminated != nil && c.State.Terminated.ExitCode == 0 {
+				stop = &c.State.Terminated.FinishedAt.Time
+			} else {
+				return nil, nil, fmt.Errorf("completion not finished yet")
+			}
+		}
+	}
+
+	if start == nil || stop == nil {
+		return nil, nil, fmt.Errorf("failed to extract time")
+	}
+
+	return start, stop, nil
+}
+
+func convertMap(orig map[string]string) map[string]interface{} {
+	res := make(map[string]interface{})
+	for k, v := range orig {
+		res[k] = v
+	}
+	return res
+}
+
+type BuilderDependencyFn func() (slsav1.ResourceDescriptor, error)
+
+type versionedObject struct {
+	Name            string `json:"name"`
+	ResourceVersion string `json:"resourceVersion"`
+}
+
+type K8sObject interface {
+	GetObjectKind() schema.ObjectKind
+	GetName() string
+	GetResourceVersion() string
+}
+
+// WithVersionedObject converts a kubernetes object to a SLSA ResourceDescriptor, where the name is
+// the Kind, and the content is the json serialzed Name and ResourceVersion of the object.
+func WithVersionedObject(obj K8sObject) BuilderDependencyFn {
+	return func() (slsav1.ResourceDescriptor, error) {
+		versioned := versionedObject{
+			Name:            obj.GetName(),
+			ResourceVersion: obj.GetResourceVersion(),
+		}
+		bytes, err := json.Marshal(versioned)
+		if err != nil {
+			return slsav1.ResourceDescriptor{}, fmt.Errorf("marshalling json: %v", err)
+		}
+
+		return slsav1.ResourceDescriptor{
+			Name:    obj.GetObjectKind().GroupVersionKind().Kind,
+			Content: bytes,
+		}, nil
+	}
+}
+
+// WithVersionedObjects is the same as WithVersionedObject but handles a slice of objects. These
+// objects must have the same GVK
+func WithVersionedObjects(objs []K8sObject) BuilderDependencyFn {
+	return func() (slsav1.ResourceDescriptor, error) {
+		kind := ""
+		versioned := make([]versionedObject, len(objs))
+		for i, obj := range objs {
+			if kind == "" {
+				kind = obj.GetObjectKind().GroupVersionKind().Kind
+			} else if kind != obj.GetObjectKind().GroupVersionKind().Kind {
+				return slsav1.ResourceDescriptor{}, fmt.Errorf("objects have different kinds")
+			}
+
+			versioned[i] = versionedObject{
+				Name:            obj.GetName(),
+				ResourceVersion: obj.GetResourceVersion(),
+			}
+		}
+		bytes, err := json.Marshal(versioned)
+		if err != nil {
+			return slsav1.ResourceDescriptor{}, fmt.Errorf("marshalling json: %v", err)
+		}
+
+		return slsav1.ResourceDescriptor{
+			Name:    kind,
+			Content: bytes,
+		}, nil
+	}
+}
---
+++ pkg/slsa/slsa_test.go
@@ -0,0 +1,279 @@
+package slsa
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/google/go-containerregistry/pkg/authn"
+	slsav1 "github.com/in-toto/in-toto-golang/in_toto/slsa_provenance/v1"
+	"github.com/sclevine/spec"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	buildv1alpha2 "github.com/pivotal/kpack/pkg/apis/build/v1alpha2"
+	corev1alpha1 "github.com/pivotal/kpack/pkg/apis/core/v1alpha1"
+	"github.com/pivotal/kpack/pkg/cnb"
+	"github.com/pivotal/kpack/pkg/config"
+)
+
+func TestImageSigner(t *testing.T) {
+	spec.Run(t, "Test SLSA generation", testSlsa)
+}
+
+func pprint(obj interface{}) error {
+	b, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(b))
+	return nil
+}
+
+func testSlsa(t *testing.T, when spec.G, it spec.S) {
+	var (
+		attester *Attester
+
+		build *buildv1alpha2.Build
+		pod   *corev1.Pod
+	)
+
+	build = &buildv1alpha2.Build{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-build-1",
+			Namespace: "default",
+		},
+		Spec: buildv1alpha2.BuildSpec{
+			Builder: corev1alpha1.BuildBuilderSpec{
+				Image: "some-registry.io/some-builder@sha256:de9964b5f501a77b8cf549659f81e29dbac4f8df7f1890ddc2b568dbed428b73",
+			},
+			Cache: &buildv1alpha2.BuildCacheConfig{
+				Volume: &buildv1alpha2.BuildPersistentVolumeCache{
+					ClaimName: "test-cache",
+				},
+			},
+			RunImage: buildv1alpha2.BuildSpecImage{
+				Image: "index.docker.io/paketobuildpacks/run-jammy-base@sha256:e817bca35911221677b678bf8bf29a18c17ce867b29bd9d0b0c3342c063854e5",
+			},
+			ServiceAccountName: "default",
+			Source: corev1alpha1.SourceConfig{
+				Git: &corev1alpha1.Git{
+					Revision: "82cb521d636b282340378d80a6307a08e3d4a4c4",
+					URL:      "https://some-git.com/org/repo.git",
+				},
+			},
+			Tags: []string{
+				"some-registry.io/some/repo",
+				"some-registry.io/some/repo:b1.20231108.210915",
+			},
+		},
+		Status: buildv1alpha2.BuildStatus{
+			LatestImage: "some-registry.io/some/repo@sha256:27227f3eaf20afcd527f31bcaaa1a10d14f30c2a99b313c86b981906c54c07b9",
+		},
+	}
+
+	pod = &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-build-1-build-pod",
+		},
+		Spec: corev1.PodSpec{
+			NodeName: "some-node",
+		},
+		Status: corev1.PodStatus{
+			InitContainerStatuses: []corev1.ContainerStatus{
+				{
+					Name: "prepare",
+					State: corev1.ContainerState{
+						Terminated: &corev1.ContainerStateTerminated{
+							ExitCode:   0,
+							Reason:     "Completed",
+							StartedAt:  metav1.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC),
+							FinishedAt: metav1.Date(2023, time.January, 1, 1, 0, 0, 0, time.UTC),
+						},
+					},
+				},
+			},
+			ContainerStatuses: []corev1.ContainerStatus{
+				{
+					Name: "completion",
+					State: corev1.ContainerState{
+						Terminated: &corev1.ContainerStateTerminated{
+							ExitCode:   0,
+							Reason:     "Completed",
+							StartedAt:  metav1.Date(2023, time.January, 1, 1, 0, 0, 0, time.UTC),
+							FinishedAt: metav1.Date(2023, time.January, 1, 2, 0, 0, 0, time.UTC),
+						},
+					},
+				}},
+		},
+	}
+
+	attester = &Attester{
+		Version: "v0.0.0",
+
+		LifecycleProvider: &fakeLifecycleProvider{},
+
+		Images: config.Images{
+			BuildInitImage: "build-init-image", BuildInitWindowsImage: "build-init-windows-image",
+			BuildWaiterImage: "build-waiter-image",
+			CompletionImage:  "completion-image", CompletionWindowsImage: "completion-windows-image",
+			RebaseImage: "rebase-image",
+		},
+		Config:   config.Config{EnablePriorityClasses: false, MaximumPlatformApiVersion: "", SshTrustUnknownHosts: true},
+		Features: config.FeatureFlags{InjectedSidecarSupport: false},
+	}
+
+	it("", func() {
+		stmt, err := attester.GenerateStatement(build, pod, authn.DefaultKeychain, authn.DefaultKeychain)
+		require.NoError(t, err)
+
+		expected := `{
+  "_type": "https://in-toto.io/Statement/v0.1",
+  "predicateType": "https://slsa.dev/provenance/v1",
+  "subject": [
+    {
+      "name": "some-registry.io/some/repo",
+      "digest": {
+        "sha256": "27227f3eaf20afcd527f31bcaaa1a10d14f30c2a99b313c86b981906c54c07b9"
+      }
+    }
+  ],
+  "predicate": {
+    "buildDefinition": {
+      "buildType": "https://github.com/buildpacks-community/kpack/blob/vv0.0.0/docs/slsa.md",
+      "externalParameters": {
+        "tags": [
+          "some-registry.io/some/repo",
+          "some-registry.io/some/repo:b1.20231108.210915"
+        ],
+        "builder": {
+          "image": "some-registry.io/some-builder@sha256:de9964b5f501a77b8cf549659f81e29dbac4f8df7f1890ddc2b568dbed428b73"
+        },
+        "serviceAccountName": "default",
+        "source": {
+          "git": {
+            "url": "https://some-git.com/org/repo.git",
+            "revision": "82cb521d636b282340378d80a6307a08e3d4a4c4"
+          }
+        },
+        "cache": {
+          "volume": {
+            "persistentVolumeClaimName": "test-cache"
+          }
+        },
+        "runImage": {
+          "image": "index.docker.io/paketobuildpacks/run-jammy-base@sha256:e817bca35911221677b678bf8bf29a18c17ce867b29bd9d0b0c3342c063854e5"
+        },
+        "resources": {}
+      },
+      "internalParameters": {
+        "builderImage": "some-registry.io/some-builder@sha256:de9964b5f501a77b8cf549659f81e29dbac4f8df7f1890ddc2b568dbed428b73",
+        "enablePriorityClasses": false,
+        "maximumPlatformApiVersion": "",
+        "sshTrustUnknownHosts": true,
+        "buildInitImage": "build-init-image",
+        "buildInitWindowsImage": "build-init-windows-image",
+        "buildWaiterImage": "build-waiter-image",
+        "completionImage": "completion-image",
+        "completionWindowsImage": "completion-windows-image",
+        "rebaseImage": "rebase-image",
+        "injectedSidecarSupport": false
+      }
+    },
+    "runDetails": {
+      "builder": {
+        "id": "",
+        "version": {
+          "kpack": "v0.0.0",
+          "lifecycle": "1.2.3"
+        }
+      },
+      "metadata": {
+        "invocationID": "https://kpack.io/default/test-build-1/test-build-1-build-pod@some-node",
+        "startedOn": "2023-01-01T00:00:00Z",
+        "finishedOn": "2023-01-01T02:00:00Z"
+      }
+    }
+  }
+}`
+
+		actual, err := json.MarshalIndent(stmt, "", "  ")
+		require.NoError(t, err)
+
+		require.Equal(t, expected, string(actual))
+	})
+
+	when("using the builder dependency fn", func() {
+		it("records single object", func() {
+			fn := WithVersionedObject(&corev1.Namespace{
+				TypeMeta: metav1.TypeMeta{Kind: "Namespace"},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:            "some-namespace",
+					ResourceVersion: "5",
+				},
+			})
+
+			actual, err := fn()
+			require.NoError(t, err)
+
+			require.Equal(t,
+				slsav1.ResourceDescriptor{
+					Name:    "Namespace",
+					Content: []byte(`{"name":"some-namespace","resourceVersion":"5"}`),
+				},
+				actual,
+			)
+		})
+
+		it("records multiple objects", func() {
+			fn := WithVersionedObjects([]K8sObject{
+				&corev1.Secret{
+					TypeMeta: metav1.TypeMeta{Kind: "Secret"},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "some-secret-1",
+						ResourceVersion: "4",
+					},
+				},
+				&corev1.Secret{
+					TypeMeta: metav1.TypeMeta{Kind: "Secret"},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "some-secret-2",
+						ResourceVersion: "10",
+					},
+				},
+				&corev1.Secret{
+					TypeMeta: metav1.TypeMeta{Kind: "Secret"},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "some-secret-3",
+						ResourceVersion: "1041",
+					},
+				},
+			})
+
+			actual, err := fn()
+			require.NoError(t, err)
+
+			require.Equal(t,
+				slsav1.ResourceDescriptor{
+					Name:    "Secret",
+					Content: []byte(`[{"name":"some-secret-1","resourceVersion":"4"},{"name":"some-secret-2","resourceVersion":"10"},{"name":"some-secret-3","resourceVersion":"1041"}]`),
+				},
+				actual,
+			)
+		})
+	})
+}
+
+type fakeLifecycleProvider struct {
+}
+
+func (l *fakeLifecycleProvider) Metadata() (cnb.LifecycleMetadata, error) {
+	return cnb.LifecycleMetadata{
+		LifecycleInfo: cnb.LifecycleInfo{
+			Version: "1.2.3",
+		},
+	}, nil
+}
