a091d9f168208bc55783b4c742edfe1dcf193dc2
Antoine Roy-Gobeil <antoine@plot.ly>
2024-03-28 16:58:30 -0400

    bump lifecycle version from 0.17.3 to 0.17.5

--- hack/lifecycle/main.go
+++ hack/lifecycle/main.go
@@ -32,7 +32,7 @@ import (
 const (
 	lifecycleMetadataLabel = "io.buildpacks.lifecycle.metadata"
 	lifecycleLocation      = "/cnb/lifecycle/"
-	lifecycleVersion       = "0.17.3"
+	lifecycleVersion       = "0.17.5"
 )
 
 var (
